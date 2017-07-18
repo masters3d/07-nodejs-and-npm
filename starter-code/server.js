7a606436395620e23398b05e25836a65a6d46dc4
J Cheyo Jimenez <cheyo@masters3d.com>
2017-07-18 16:40:39 -0700

    added express and static

--- starter-code/server.js
+++ starter-code/server.js
@@ -11,8 +11,12 @@
 
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
 // TODO: Include all of the static resources as an argument to app.use()
+// expected to take 10 mins but it took 5 minutes
+app.use(express.static('./public'));
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
